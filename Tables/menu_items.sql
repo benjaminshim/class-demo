d4dab397a6256d2f9f3f9d341b3277e333eb5e5f
benjaminshim <benjaminshim2002@gmail.com>
2023-10-29 23:45:18 -0400

    add values into reservations table, created menu_items.sql

--- Tables/menu_items.sql
+++ Tables/menu_items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2023 at 01:08 PM
+-- Generation Time: Oct 29, 2023 at 03:26 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -27,30 +27,20 @@ SET time_zone = "+00:00";
 -- Table structure for table `restaurant_reservations`
 --
 
-CREATE TABLE `reservations` (
-  `ReservationID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `menu_items` (
+  `MenuItemID` int(11) NOT NULL AUTO_INCREMENT,
   `RestaurantID` int(11) NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `ReservationDate` date NOT NULL,
-  `ReservationTime` time NOT NULL,
-  `PartySize` int(5) NOT NULL,
-  PRIMARY KEY (`ReservationID`),
-  FOREIGN KEY (`RestaurantID`) REFERENCES `one_star_michelin_restaurants`(`ID`),
-  FOREIGN KEY (`UserID`) REFERENCES `users`(`UserID`)
+  `ItemName` varchar(255) NOT NULL,
+  `Description` text,
+  `Price` decimal(10, 2) NOT NULL,
+  `Category` varchar(50),
+  -- Add more menu item details as needed
+  PRIMARY KEY (`MenuItemID`),
+  FOREIGN KEY (`RestaurantID`) REFERENCES `restaurants`(`RestaurantID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
--- Dumping data for table `one_star_michelin_restaurants`
---
-
-INSERT INTO `restaurant_reservations` (`RestaurantID`, `UserID`, `ReservationDate`, `ReservationTime`, `PartySize`) VALUES
-(1, 1, '2023-10-30', '19:00:00', 2),
-(2, 2, '2023-11-05', '20:00:00', 4),
-(3, 3, '2023-11-12', '18:30:00', 3),
-(4, 4, '2023-11-15', '19:30:00', 5);
-COMMIT;
+-- --------------------------------------------------------
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-

b4f1273f7b971c08f1e90d21b23a7d51d89c2766
Carolina Martin <carolinamartin@10-21-202-232.dynapool.wireless.nyu.edu>
2023-11-05 21:47:34 -0500

    add customers.py which will help interact with the customer's data

---
+++ data/customers.py
@@ -0,0 +1,17 @@
+"""
+This module interfaces to our customer data
+"""
+
+USERNAME = 'Username'
+MIN_CUST_NAME_LEN = 2
+
+def get_customer():
+  customers = {
+    "Andy": {
+      USERNAME: 'andy123',
+    },
+    "Benji": {
+      USERNAME: 'benji234',
+    },
+  }
+  return customers

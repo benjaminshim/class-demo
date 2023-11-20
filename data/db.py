5799b607c976b4e4ad755fc3ab6c69238a2e73b9
benjaminshim <benjaminshim2002@gmail.com>
2023-11-20 00:05:22 -0500

    fixing build by changing delete function

--- data/db.py
+++ data/db.py
@@ -96,11 +96,11 @@ def add_user(name: str, id: int) -> bool:
     return _gen_id()
 
 
-def del_restaurant(name: str):
-    if exists(name):
-        del restaurants[name]
-    else:
-        raise ValueError(f'Delete failure: {name} not in database.')
+# def del_restaurant(name: str):
+#     if exists(name):
+#         del restaurants[name]
+#     else:
+#         raise ValueError(f'Delete failure: {name} not in database.')
     
 def add_user(name: str, id: int) -> bool:
     if id in users:

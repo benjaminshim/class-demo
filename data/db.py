421b962b446dab9be33b63ea2082280a9e56cd05
Carolina Martin <carolinamartin@10-21-159-169.dynapool.wireless.nyu.edu>
2023-11-28 19:21:22 -0500

    changing del to return in del_restaurant

--- data/db.py
+++ data/db.py
@@ -108,7 +108,7 @@ def add_user(name: str, id: int) -> bool:
 
 def del_restaurant(name: str):
     if exists(name):
-        del dbc.del_one(RESTAURANT_COLLECT, {NAME: name})
+        return dbc.del_one(RESTAURANT_COLLECT, {NAME: name})
     else:
         raise ValueError(f'Delete failure: {name} not in database.')
 

99998033ed8657cc845489c603e3495810ae0a17
benjaminshim <benjaminshim2002@gmail.com>
2023-12-03 21:40:12 -0500

    add add_user() to users.py

--- data/users.py
+++ data/users.py
@@ -1,6 +1,20 @@
 """
 This module interfaces to our user data
 """
+import random
+
+import data.db_connect as dbc
+import data.users as usrs
+
+
+BIG_NUM = 10000000000000
+ID_LEN = 24
+MOCK_ID = '0' * ID_LEN
+
+NAME = 'name'
+
+USER_NAME = "User"
+USER_ID = "_id"
 
 import data.db_connect as dbc
 
@@ -12,3 +26,23 @@ USERS_COLLECT = 'users'
 def get_users() -> dict:
     dbc.connect_db()
     return dbc.fetch_all_as_dict(USERNAME, USERS_COLLECT)
+
+
+def _gen_id() -> str:
+    _id = random.randint(0, BIG_NUM)
+    _id = str(_id)
+    _id = _id.rjust(ID_LEN, '0')
+    return _id
+
+
+def add_user(name: str, id: int) -> bool:
+    users = {}
+    if id in users:
+        raise ValueError(f'Duplicate user id: {id=}')
+    if not id:
+        raise ValueError('Users are not allowed to be entered without ids')
+    users[USER_ID] = _gen_id()
+    users[USER_NAME] = name
+    dbc.connect_db()
+    _id = dbc.insert_one(usrs.USERS_COLLECT, users)
+    return _id is not None

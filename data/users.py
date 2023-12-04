ed56c20de1b6de4419dd7beb8dd77d08e5fd9e80
benjaminshim <benjaminshim2002@gmail.com>
2023-12-03 21:25:15 -0500

    deleting test data for users (since we've connected to MongoDB)

--- data/users.py
+++ data/users.py
@@ -12,21 +12,3 @@ USERS_COLLECT = 'users'
 def get_users() -> dict:
     dbc.connect_db()
     return dbc.fetch_all_as_dict(USERNAME, USERS_COLLECT)
-
-
-# def get_old_users():
-#     users = {
-#         "Jason": {
-#             USERNAME: 'jl1002',
-#         },
-#         "Natalie": {
-#             USERNAME: 'nw2003',
-#         },
-#         "Kevin": {
-#             USERNAME: 'ks3004',
-#         },
-#         "Chris": {
-#             USERNAME: 'ch4005',
-#         },
-#     }
-#     return users

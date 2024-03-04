810f0ac0afb021b52ede951abfec1303dd24b321
AndyCQ <acq3433@nyu.edu>
2024-03-03 23:23:19 -0500

    Added tests for update username function

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -31,3 +31,21 @@ def test_get_users():
 def test_del_user(mock_del):
     resp = TEST_CLIENT.delete(f'{ep.RESTAURANTS_EP}/AnyName')
     assert resp.status_code == OK
+
+
+@patch('data.users.update_username', side_effect=ValueError(), autospec=True)
+def test_bad_update_username(mock_update):
+    """
+    Testing we do the right thing with a call to update_rating that fails.
+    """
+    resp = TEST_CLIENT.put(f'{ep.USERS_EP}/AnyName/100')
+    assert resp.status_code == NOT_FOUND
+
+
+@patch('data.users.update_username', autospec=True)
+def test_update_username(mock_update):
+    """
+    Testing we do the right thing with a call to update_rating that succeeds.
+    """
+    resp = TEST_CLIENT.put(f'{ep.USERS_EP}/AnyName/100')
+    assert resp.status_code == OK
